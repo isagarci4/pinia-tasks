83ad9fe695ba3f417b5fc18daa0da9d5a6d2142e
Isadora Garcia <isadoragarciaalmeidasilva7450@gmail.com>
2023-10-25 16:18:38 -0300

    feat: adding clicks to delete and favorite a task

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
+import { useTaskStore } from '../stores/TaskStore';
+
     const props = defineProps({
         task: {
             type: Object,
             required: true,
-        }
+        },
     })
+
+    const taskStore = useTaskStore()
 </script>
 
 <template>
     <div class="task">
         <h3>{{ props.task.title }}</h3>
         <div class="icons">
-            <i class="material-icons">delete</i>
-            <i class="material-icons">favorite</i>
+            <i class="material-icons" @click="taskStore.deleteTask(props.task.id)">delete</i>
+            <i class="material-icons" :class="{active: props.task.isFav}" @click="taskStore.toggleFav(props.task.id)">favorite</i>
         </div>
     </div>
 </template>
